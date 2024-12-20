58828b0cdbccb8920944bf20b5033db5eb46a44b
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-20 21:39:27 +0530

    Added Sidebar image

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,40 @@
+import { Box } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <Grid container spacing={0}>
+      <Grid
+        size={{ xs: 0, md: 6 }}
+        sx={{ position: "relative", height: "100vh" }}
+      >
+        <Box
+          component="img"
+          src="/assets/hero-rabbit-img.png"
+          sx={{
+            width: "284px",
+            height: "auto",
+            position: "absolute",
+            bottom: 0,
+          }}
+        />
+        <Box
+          component="img"
+          src="/assets/hero-stats-img.png"
+          sx={{
+            width: "29rem",
+            height: "auto",
+            position: "absolute",
+            zIndex: 2,
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%,-50%)",
+          }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>Item 2</Grid>
+    </Grid>
+  );
+};
 
-export default SignIn
+export default SignIn;
