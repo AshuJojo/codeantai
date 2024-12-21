710847c03eddba81515b314b38a3537388d3445b
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-21 11:50:04 +0530

    Updated MUI Theme

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 
 const SignIn = () => {
@@ -6,31 +6,41 @@ const SignIn = () => {
     <Grid container spacing={0}>
       <Grid
         size={{ xs: 0, md: 6 }}
-        sx={{ position: "relative", height: "100vh" }}
+        sx={{
+          height: "100vh",
+        }}
       >
-        <Box
-          component="img"
-          src="/assets/hero-rabbit-img.png"
+        <Paper
           sx={{
-            width: "284px",
-            height: "auto",
-            position: "absolute",
-            bottom: 0,
+            position: "relative",
+            borderRadius: 0,
+            height: "100%",
           }}
-        />
-        <Box
-          component="img"
-          src="/assets/hero-stats-img.png"
-          sx={{
-            width: "29rem",
-            height: "auto",
-            position: "absolute",
-            zIndex: 2,
-            left: "50%",
-            top: "50%",
-            transform: "translate(-50%,-50%)",
-          }}
-        />
+        >
+          <Box
+            component="img"
+            src="/assets/hero-rabbit-img.png"
+            sx={{
+              width: "284px",
+              height: "auto",
+              position: "absolute",
+              bottom: 0,
+            }}
+          />
+          <Box
+            component="img"
+            src="/assets/hero-stats-img.png"
+            sx={{
+              width: "29rem",
+              height: "auto",
+              position: "absolute",
+              zIndex: 2,
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%,-50%)",
+            }}
+          />
+        </Paper>
       </Grid>
       <Grid size={{ xs: 12, md: 6 }}>Item 2</Grid>
     </Grid>
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,32 @@
 import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    primary: {
+      dark: "#175CD3",
+      main: "#1570EF",
+      light: "#EFF8FF",
+      contrastText: "#FFFFFF",
+    },
+    secondary: {
+      main: "#FFFFFF",
+      contrastText: "#171717",
+    },
+  },
+  components: {
+    MuiPaper: {
+      defaultProps: {
+        elevation: 0,
+      },
+      styleOverrides: {
+        root: {
+          backgroundColor: "#FFFFFF",
+          border: "1px solid #E9EAEB",
+          borderRadius: "12px",
+        },
+      },
+    },
+  },
+});
 
 export default theme;
