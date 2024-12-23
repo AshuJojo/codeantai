23acee2a799f2b70dfd74533759a2de3b719fe60
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-23 22:37:35 +0530

    Updated Sidebar to handle reponsivness of small screens

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,51 +1,35 @@
 import {
+  alpha,
   Box,
   MenuItem,
   MenuList,
   Select,
   Stack,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import { BiBook, BiCloud, BiCodeAlt, BiHome } from "react-icons/bi";
 import { BsTelephone } from "react-icons/bs";
 import { FiLogOut } from "react-icons/fi";
-import { IoSettingsOutline } from "react-icons/io5";
+import { IoClose, IoSettingsOutline } from "react-icons/io5";
 import { Link } from "react-router";
 import StyledMenuItem from "./StyledMenu";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Sidebar = () => {
-  return (
-    <Stack
-      direction="column"
-      sx={{
-        bgcolor: "white",
-        height: "100vh",
-        px: 2,
-        py: 3,
-        borderRight: "1px solid #E9EAEB",
-      }}
-    >
-      <Stack
-        direction="column"
-        spacing={2.5}
-        sx={{ px: 0.5, width: "100%", alignItems: "start" }}
-      >
-        <Link to={"/"}>
-          <Box component="img" src="/logo-long.svg" height={32} width="auto" />
-        </Link>
-        <Select
-          value={0}
-          defaultValue={0}
-          size="small"
-          sx={{ textAlign: "start", borderRadius: 2 }}
-          fullWidth
-        >
-          <MenuItem value={0}>Ashutosh Sharma</MenuItem>
-          <MenuItem value={1}>Add User</MenuItem>
-        </Select>
-      </Stack>
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+  const [menuOpen, setMenuOpen] = useState<boolean>(true);
 
-      {/* Menu */}
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  // Menu List Component
+  const getMenuList = () => {
+    return (
       <MenuList
         component="nav"
         sx={{
@@ -54,7 +38,6 @@ const Sidebar = () => {
           flexDirection: "column",
           flexGrow: 1,
           justifyContent: "space-between",
-          mt: 2,
           p: 0,
         }}
       >
@@ -128,6 +111,116 @@ const Sidebar = () => {
           </StyledMenuItem>
         </Stack>
       </MenuList>
+    );
+  };
+
+  return (
+    <Stack
+      direction="column"
+      sx={{
+        bgcolor: "white",
+        height: { xs: "64px", md: "100vh" },
+        px: { xs: 0, md: 2 },
+        py: { xs: 0, md: 3 },
+        borderRight: "1px solid #E9EAEB",
+      }}
+    >
+      <Stack
+        direction="column"
+        spacing={{ xs: 0, md: 2.5 }}
+        sx={{
+          px: { xs: 0, md: 0.5 },
+          width: "100%",
+          height: "100%",
+          alignItems: "start",
+        }}
+      >
+        <Stack
+          direction="row"
+          sx={{
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+            p: { xs: 2, md: 0 },
+          }}
+        >
+          {/* Logo */}
+          <Link to={"/"}>
+            <Box
+              component="img"
+              src="/logo-long.svg"
+              height={32}
+              width="auto"
+            />
+          </Link>
+
+          {/* Menu Button */}
+          {!isDesktop && (
+            <>
+              {menuOpen ? (
+                <IoClose size={24} onClick={handleToggleMenu} />
+              ) : (
+                <RxHamburgerMenu size={24} onClick={handleToggleMenu} />
+              )}
+            </>
+          )}
+        </Stack>
+        {/* User  */}
+        {(menuOpen || isDesktop) && (
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              position: "relative",
+              top: 0,
+              bottom: 0,
+              m: 0,
+            }}
+          >
+            <Stack
+              spacing={2}
+              sx={{
+                width: "100%",
+                height: { xs: "450px", md: "100%" },
+                position: "absolute",
+                bgcolor: "white",
+                px: { xs: 2, md: 0 },
+                py: { xs: 2, md: 0 },
+                zIndex: 1,
+                left: 0,
+                right: 0,
+              }}
+            >
+              <Select
+                value={0}
+                defaultValue={0}
+                size="small"
+                sx={{ textAlign: "start", borderRadius: 2 }}
+                fullWidth
+              >
+                <MenuItem value={0}>Ashutosh Sharma</MenuItem>
+                <MenuItem value={1}>Add User</MenuItem>
+              </Select>
+
+              {/* Menu */}
+              {getMenuList()}
+            </Stack>
+            {!isDesktop && (
+              <Box
+                sx={{
+                  height: "100vh",
+                  bgcolor: alpha("#000000", 0.5),
+                  position: "absolute",
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                }}
+              />
+            )}
+          </Box>
+        )}
+      </Stack>
     </Stack>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,10 +5,10 @@ import Sidebar from "../components/Sidebar";
 const Dashboard = () => {
   return (
     <Grid container sx={{ height: "100vh", textAlign: "start" }}>
-      <Grid size={{ xs: 4, md: 3, lg: 2 }}>
+      <Grid size={{ xs: 12, md: 3, lg: 2 }}>
         <Sidebar />
       </Grid>
-      <Grid size={{ xs: 8, md: 9, lg: 10 }} p={3}>
+      <Grid size={{ xs: 12, md: 9, lg: 10 }} p={{ xs: 0, md: 3 }}>
         <RepositoriesCard />
       </Grid>
     </Grid>
